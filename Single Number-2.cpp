b4c9fcf482a2b4996d0207951185ea35b5209efb
Mohit Kumar <148743546+MateeRixx@users.noreply.github.com>
2025-10-02 13:36:52 +0530

    Create Single Number-2.cpp

---
+++ Single Number-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans=0;
+        for(int bit_index=0;bit_index<32;bit_index++)
+        {
+            int count=0;
+            for(int i=0;i<nums.size();i++)
+            {
+                if(nums[i] & (1<<bit_index)){
+                    count++;
+
+                }
+            }
+
+            if(count%3==1)
+            {
+                ans=ans | (1<<bit_index);
+
+            }
+        }
+
+    return ans;
+
+        
+    }
+};
